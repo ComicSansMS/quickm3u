94d5e2a33a27e894a8d299cb8b5d640889bcee53
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-10-20 22:06:52 +0200

    windows crash dumps

--- src/quickm3u/main.cpp
+++ src/quickm3u/main.cpp
@@ -9,8 +9,41 @@
 #pragma warning(pop)
 #endif
 
+#ifdef WIN32
+#include <Windows.h>
+#include <Dbghelp.h>
+#endif
+
+namespace {
+#ifdef WIN32
+LONG WINAPI win32CrashHandler(EXCEPTION_POINTERS* exceptionInfo)
+{
+    HANDLE hfile = CreateFile("crash.dmp", GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
+    if (hfile != INVALID_HANDLE_VALUE) {
+        MINIDUMP_EXCEPTION_INFORMATION md_exc_info;
+        md_exc_info.ThreadId = GetCurrentThreadId();
+        md_exc_info.ExceptionPointers = exceptionInfo;
+        md_exc_info.ClientPointers = TRUE;
+        MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hfile,
+                          MiniDumpNormal, &md_exc_info, nullptr, nullptr);
+    }
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+void prepareCrashHandler()
+{
+    SetUnhandledExceptionFilter(win32CrashHandler);
+}
+#else
+void prepareCrashHandler()
+{
+}
+#endif
+}
+
 int main(int argc, char* argv[])
 {
+    prepareCrashHandler();
     QApplication the_app(argc, argv);
 
     ui::MainWindow main_window;
